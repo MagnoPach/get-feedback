26db64ff30153f035d10dc35938eac3929b8f799
magnopachecoluiz <magno.luiz@nicephotos.net>
2022-06-01 09:29:10 -0300

    Edit footer text

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -64,7 +64,7 @@ export function WidgetForm() {
         </>
       )}
       <footer>
-        Visite meu
+        Visite meu{" "}
         <a
           href="https://github.com/MagnoPach"
           className="underline underline-offset-2"
